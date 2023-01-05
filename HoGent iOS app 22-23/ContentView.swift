f30ef08a7489e511c776c7efea98b9f97537a89d
Jens Penneman <jenspenneman26@gmail.com>
2023-01-05 20:15:51 +0100

    Initial Commit

---
+++ HoGent iOS app 22-23/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HoGent iOS app 22-23
+//
+//  Created by Jens Penneman on 05/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HoGent iOS app 22-23/HoGent_iOS_app_22_23App.swift
@@ -0,0 +1,17 @@
+//
+//  HoGent_iOS_app_22_23App.swift
+//  HoGent iOS app 22-23
+//
+//  Created by Jens Penneman on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HoGent_iOS_app_22_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
